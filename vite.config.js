6b997d26ee08716d3172463ed78654abb70551a0
anirudhkrishnatreya <80411739+anirudhkrishnatreya@users.noreply.github.com>
2026-04-19 23:54:39 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/client/',   // ✅ MOVE IT HERE (TOP LEVEL)
+  base: '/',   // ✅ MOVE IT HERE (TOP LEVEL)
   server: {
     port: 5173,
     proxy: {
